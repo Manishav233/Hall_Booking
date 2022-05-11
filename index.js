66672907cc961f45d7101cc83f05a9cd84bdc294
Manishav233 <manishav233@gmail.com>
2022-05-11 20:21:19 +0530

    code updated

--- index.js
+++ index.js
@@ -120,7 +120,46 @@ console.log("booking a room")
 
 })
 
+//List all rooms with booked data
+app.get("/rooms", (request, response) => {
+  response.send(
+    rooms.map((room) => {
+      if (room.bookedStatus == true) {
+        return {
+          "Room name": room.roomName,
+          "Booked Status": "Booked",
+          "Customer Name": room.customerDetails.customerName,
+          "Date": room.customerDetails.date,
+          "Start Time": room.customerDetails.startTime,
+          "End Time": room.customerDetails.endTime,
+        };
+      } else {
+        return { "Room name": room.roomName, "Booked Status": "Vacant" };
+      }
+    })
+  );
+});
 
+//List all customers with booked data
+app.get("/customers", (request, response) => {
+  response.send(
+    rooms
+      .filter((room) => {
+        if (room.bookedStatus === true) {
+          return room;
+        }
+      })
+      .map((room) => {
+        return {
+          "Customer name": room.customerDetails.customerName,
+          "Room name": room.roomName,
+          Date: room.customerDetails.date,
+          "Start Time": room.customerDetails.startTime,
+          "End Time": room.customerDetails.endTime,
+        };
+      })
+  );
+});
 
 //specifying port to use
 app.listen(PORT, () => console.log("server has started at port", PORT));
